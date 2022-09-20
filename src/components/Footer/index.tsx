dc7a2c9aab51d9a013107e4ac572b64c6cb6a971
Welton Nogueira <welton.nogueira.dev@gmail.com>
2022-09-20 00:52:34 -0300

    Edited

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2020 - Developed by <a href="https://github.com/nogueirawelton">Welton Nogueira</a></p>
+      <p>&copy; 2022 - Developed by <a href="https://github.com/nogueirawelton">Welton Nogueira</a></p>
     </footer>
   )
 }
